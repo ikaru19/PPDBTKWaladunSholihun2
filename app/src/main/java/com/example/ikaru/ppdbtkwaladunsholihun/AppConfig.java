38fb1abddca37a11e61c7163fc1f63369b60c8f2
ikaru19 <syafrizalizal17@gmail.com>
2018-08-18 19:58:19 +0700

    First Commit

---
+++ app/src/main/java/com/example/ikaru/ppdbtkwaladunsholihun/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.ikaru.ppdbtkwaladunsholihun;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://tkikaru.000webhostapp.com/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://tkikaru.000webhostapp.com/register.php";
+}
