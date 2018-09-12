5ecf5189453bde2d17805d72dee1a164781063b6
Muhammad Syafrizal <syafrizalizal17@gmail.com>
2018-09-12 21:38:16 +0700

    Update AppConfig.java

--- app/src/main/java/com/example/ikaru/ppdbtkwaladunsholihun/AppConfig.java
+++ app/src/main/java/com/example/ikaru/ppdbtkwaladunsholihun/AppConfig.java
@@ -2,8 +2,8 @@ package com.example.ikaru.ppdbtkwaladunsholihun;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://tkikaru.000webhostapp.com/login.php";
+    public static String URL_LOGIN = "http://dimasprasetya.000webhostapp.com/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://tkikaru.000webhostapp.com/register.php";
+    public static String URL_REGISTER = "http://dimasprasetya.000webhostapp.com/register.php";
 }
